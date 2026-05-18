5a2fb41256d28380f6fb3562f659241489cf89b8
Reshma66-web <reshma88450@gmail.com>
2026-05-18 23:11:04 +0530

    Initial commit

---
+++ tests/login.spec.js
@@ -0,0 +1,9 @@
+test("fill and type",async({page})=>{
+    await page.goto("https://www.saucedemo.com/")
+    // ! fill
+    await page.getByTestId("username").fill("standard_user")
+    // ! type
+    await page.getByTestId("password").type("secret_sauce")
+    // ! click()
+    await page.getByTestId("login-button").click()
+})
