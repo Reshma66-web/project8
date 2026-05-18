6fcc9d97608ffb4ffc73281f0c391b2ab6ac05c9
Reshma66-web <reshma88450@gmail.com>
2026-05-18 23:54:50 +0530

    branch1

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -3,6 +3,7 @@ import {test} from "@playwright/test"
 test("fill and type",async({page})=>{
     // team member 1
     // first team
+    // t1
     await page.goto("https://www.saucedemo.com/")
     
     await page.locator('//input[@id="user-name"]').fill("standard_user")
