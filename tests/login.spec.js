e3251b43d4b21b431a961e2a0a0c4f58414b09da
Reshma66-web <reshma88450@gmail.com>
2026-05-18 23:26:31 +0530

    third commit

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -2,10 +2,10 @@ import {test} from "@playwright/test"
 
 test("fill and type",async({page})=>{
     await page.goto("https://www.saucedemo.com/")
-    // ! fill
-    await page.getByTestId("username").fill("standard_user")
-    // ! type
-    await page.getByTestId("password").type("secret_sauce")
-    // ! click()
-    await page.getByTestId("login-button").click()
+    
+    await page.locator('//input[@id="user-name"]').fill("standard_user")
+    
+    await page.locator('//input[@id="password"]').fill("secret_sauce")
+    
+    await page.locator('//input[@id="login-button"]').click()
 })
